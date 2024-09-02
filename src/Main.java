41f680fe50e2352537094267c2a8161bbf879f01
lul248 <luliatekie22@gmail.com>
2024-09-02 23:40:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is: " + sum);
+    }
+}
